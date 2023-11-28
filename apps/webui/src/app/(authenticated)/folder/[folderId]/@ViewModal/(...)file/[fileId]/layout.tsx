47064baee4cdbc06e8890bcc602fd240ca7d3fab
Michael Wang <41971562+devmwang@users.noreply.github.com>
2023-11-28 14:44:24 -0800

    file view modal only rounded when viewport >= xl

--- apps/webui/src/app/(authenticated)/folder/[folderId]/@ViewModal/(...)file/[fileId]/layout.tsx
+++ apps/webui/src/app/(authenticated)/folder/[folderId]/@ViewModal/(...)file/[fileId]/layout.tsx
@@ -3,7 +3,7 @@ import Modal from "@/components/file-system/modal";
 export default function FileViewModalLayout({ children }: { children: React.ReactNode }) {
     return (
         <Modal>
-            <div className="grid grid-rows-file-view-modal h-full w-full rounded-xl bg-zinc-50 dark:bg-zinc-900">{children}</div>
+            <div className="grid grid-rows-file-view-modal h-full w-full xl:rounded-xl bg-zinc-50 dark:bg-zinc-900">{children}</div>
         </Modal>
     );
 }
